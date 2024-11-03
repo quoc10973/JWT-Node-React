b8a4ba564cbe1cee2c5efe6b350df6c05545a4fe
quoc10973 <rumble01667057271@gmail.com>
2024-11-04 04:51:11 +0700

    env dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import axios from "axios"
+import { useEffect } from "react"
 
 function App() {
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/`)
+      console.log("check response", response)
+      // const data = await response.json()
+      // console.log
+    }
+
+    fetchData()
+  }, [])
 
   return (
     <>
