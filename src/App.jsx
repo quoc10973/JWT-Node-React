f374b6dd5cf06c48c98fc8537e91c782c4170dda
quoc10973 <rumble01667057271@gmail.com>
2024-11-04 04:57:53 +0700

    base interceptors

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import axios from "axios"
+import axios from "./util/axios.customize"
 import { useEffect } from "react"
 
 function App() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/`)
+      const response = await axios.get(`/api/`)
       console.log("check response", response)
       // const data = await response.json()
       // console.log
---
+++ src/util/axios.customize.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+const instance = axios.create({
+    //Set the base url to the backend url
+    baseURL: import.meta.env.VITE_BACKEND_URL
+});
+
+// Alter defaults after instance has been created
+// instance.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+
+export default instance;
+
+// Add a request interceptor
+instance.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    return config;
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+});
+
+// Add a response interceptor
+instance.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+}, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+});
