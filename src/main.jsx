6ceffb2d6ec69cafa9233676d99cb0597921cb7f
quoc10973 <rumble01667057271@gmail.com>
2024-11-04 14:43:40 +0700

    router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import "./style/global.css"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import RegisterPage from './pages/register.jsx';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+    // children: [
+    //   {
+    //     path: "register",
+    //     element: <RegisterPage />
+    //   }
+    // ]
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
